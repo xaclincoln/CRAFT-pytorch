a37b9e2099cdc66c1c0d9f1617b9543cab71e80c
lincoln <359972883@qq.com>
2021-08-31 18:10:42 +0800

    add resnet50 as backbone of craft network

--- basenet/vgg16_bn.py
+++ basenet/vgg16_bn.py
@@ -1,23 +1,11 @@
+from basenet.weight_init import init_weights
 from collections import namedtuple
 
 import torch
 import torch.nn as nn
-import torch.nn.init as init
 from torchvision import models
 from torchvision.models.vgg import model_urls
 
-def init_weights(modules):
-    for m in modules:
-        if isinstance(m, nn.Conv2d):
-            init.xavier_uniform_(m.weight.data)
-            if m.bias is not None:
-                m.bias.data.zero_()
-        elif isinstance(m, nn.BatchNorm2d):
-            m.weight.data.fill_(1)
-            m.bias.data.zero_()
-        elif isinstance(m, nn.Linear):
-            m.weight.data.normal_(0, 0.01)
-            m.bias.data.zero_()
 
 class vgg16_bn(torch.nn.Module):
     def __init__(self, pretrained=True, freeze=True):
--- craft.py
+++ craft.py
@@ -8,7 +8,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from basenet.vgg16_bn import vgg16_bn, init_weights
+#from basenet.vgg16_bn import vgg16_bn, init_weights
+from basenet.resnet50_bn import resnet50_bn, init_weights
 
 class double_conv(nn.Module):
     def __init__(self, in_ch, mid_ch, out_ch):
@@ -32,13 +33,13 @@ class CRAFT(nn.Module):
         super(CRAFT, self).__init__()
 
         """ Base network """
-        self.basenet = vgg16_bn(pretrained, freeze)
+        self.basenet = resnet50_bn(pretrained, freeze)
 
         """ U network """
-        self.upconv1 = double_conv(1024, 512, 256)
-        self.upconv2 = double_conv(512, 256, 128)
-        self.upconv3 = double_conv(256, 128, 64)
-        self.upconv4 = double_conv(128, 64, 32)
+        self.upconv1 = double_conv(2048, 1024, 512)
+        self.upconv2 = double_conv(512, 512, 256)
+        self.upconv3 = double_conv(256, 256, 64)
+        self.upconv4 = double_conv(64, 64, 32)
 
         num_class = 2
         self.conv_cls = nn.Sequential(
@@ -60,7 +61,8 @@ class CRAFT(nn.Module):
         sources = self.basenet(x)
 
         """ U network """
-        y = torch.cat([sources[0], sources[1]], dim=1)
+        y = F.interpolate(sources[0], size=sources[1].size()[2:], mode='bilinear', align_corners=False)
+        y = torch.cat([y, sources[1]], dim=1)
         y = self.upconv1(y)
 
         y = F.interpolate(y, size=sources[2].size()[2:], mode='bilinear', align_corners=False)
@@ -80,6 +82,6 @@ class CRAFT(nn.Module):
         return y.permute(0,2,3,1), feature
 
 if __name__ == '__main__':
-    model = CRAFT(pretrained=True).cuda()
-    output, _ = model(torch.randn(1, 3, 768, 768).cuda())
+    model = CRAFT(pretrained=False)
+    output, _ = model(torch.randn(1, 3, 768, 768))
     print(output.shape)
