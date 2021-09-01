a775957606d9af7c172292f778b64fd4e2f464b1
lincoln <359972883@qq.com>
2021-09-01 09:33:59 +0800

    add resnet18

--- basenet/resnet_bn.py
+++ basenet/resnet_bn.py
@@ -1,19 +1,17 @@
 from basenet.weight_init import init_weights
-from collections import namedtuple
-
 import torch
-import torch.nn as nn
 from torchvision import models
 from torchvision.models.resnet import model_urls
 
 
-class resnet50_bn(torch.nn.Module):
-    def __init__(self, pretrained=True, freeze=True):
-        super(resnet50_bn, self).__init__()
-        model_urls['resnet50'] = model_urls['resnet50'].replace('https://', 'http://')
-        self.bn = models.resnet50(pretrained=pretrained)
-    
+class resnet_bn(torch.nn.Module):
+    def __init__(self, arch: str = 'resnet18', pretrained=True, freeze=True):
+        super(resnet_bn, self).__init__()
+        model_urls[arch] = model_urls[arch].replace('https://', 'http://')
+        self.bn = getattr(models,arch)(pretrained=pretrained)
+
         if not pretrained:
+            init_weights(self.bn.conv1.modules())
             init_weights(self.bn.layer1.modules())
             init_weights(self.bn.layer2.modules())
             init_weights(self.bn.layer3.modules())
@@ -24,20 +22,20 @@ class resnet50_bn(torch.nn.Module):
     def forward(self, x):
         x = self.bn.conv1(x)
         x = self.bn.bn1(x)
-        x = self.bn.relu(x)
-        layer0 = self.bn.maxpool(x)
+        layer0 = self.bn.relu(x)
+        #x = self.bn.maxpool(x)
 
         layer1 = self.bn.layer1(layer0)
         layer2 = self.bn.layer2(layer1)
         layer3 = self.bn.layer3(layer2)
         layer4 = self.bn.layer4(layer3)
 
-        return (layer4,layer3,layer2,layer1,layer0)
+        return (layer4, layer3, layer2, layer1, layer0)
 
 
-if __name__=="__main__":
-    resnet = resnet50_bn(pretrained=False)
-    x = torch.rand([2,3,800,600])
+if __name__ == "__main__":
+    resnet = resnet_bn(pretrained=False,arch='resnet18')
+    x = torch.rand([2, 3, 800, 600])
     y = resnet(x)
     for layer in y:
         print(layer.size())
--- craft.py
+++ craft.py
@@ -9,7 +9,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 #from basenet.vgg16_bn import vgg16_bn, init_weights
-from basenet.resnet50_bn import resnet50_bn, init_weights
+from basenet.resnet_bn import resnet_bn
+from basenet.weight_init import init_weights
 
 class double_conv(nn.Module):
     def __init__(self, in_ch, mid_ch, out_ch):
@@ -29,17 +30,23 @@ class double_conv(nn.Module):
 
 
 class CRAFT(nn.Module):
-    def __init__(self, pretrained=False, freeze=False):
+    def __init__(self, pretrained=False, freeze=False, bn_arch='resnet18'):
         super(CRAFT, self).__init__()
 
         """ Base network """
-        self.basenet = resnet50_bn(pretrained, freeze)
+        self.basenet = resnet_bn(pretrained=pretrained,freeze=freeze,arch=bn_arch)
 
         """ U network """
-        self.upconv1 = double_conv(2048, 1024, 512)
-        self.upconv2 = double_conv(512, 512, 256)
-        self.upconv3 = double_conv(256, 256, 64)
-        self.upconv4 = double_conv(64, 64, 32)
+        if bn_arch=='resnet50':
+            self.upconv1 = double_conv(2048, 1024, 512)
+            self.upconv2 = double_conv(512, 512, 256)
+            self.upconv3 = double_conv(256, 256, 64)
+            self.upconv4 = double_conv(64, 64, 32)
+        elif bn_arch=='resnet18':
+            self.upconv1 = double_conv(512, 256, 128)
+            self.upconv2 = double_conv(128, 128, 64)
+            self.upconv3 = double_conv(64, 64, 32)
+            self.upconv4 = None
 
         num_class = 2
         self.conv_cls = nn.Sequential(
@@ -53,7 +60,8 @@ class CRAFT(nn.Module):
         init_weights(self.upconv1.modules())
         init_weights(self.upconv2.modules())
         init_weights(self.upconv3.modules())
-        init_weights(self.upconv4.modules())
+        if self.upconv4 is not None:
+            init_weights(self.upconv4.modules())
         init_weights(self.conv_cls.modules())
         
     def forward(self, x):
@@ -73,15 +81,18 @@ class CRAFT(nn.Module):
         y = torch.cat([y, sources[3]], dim=1)
         y = self.upconv3(y)
 
-        y = F.interpolate(y, size=sources[4].size()[2:], mode='bilinear', align_corners=False)
-        y = torch.cat([y, sources[4]], dim=1)
-        feature = self.upconv4(y)
+        if self.upconv4 is not None:
+            y = F.interpolate(y, size=sources[4].size()[2:], mode='bilinear', align_corners=False)
+            y = torch.cat([y, sources[4]], dim=1)
+            feature = self.upconv4(y)
+        else:
+            feature = y
 
         y = self.conv_cls(feature)
 
         return y.permute(0,2,3,1), feature
 
 if __name__ == '__main__':
-    model = CRAFT(pretrained=False)
+    model = CRAFT(pretrained=False,bn_arch='resnet18')
     output, _ = model(torch.randn(1, 3, 768, 768))
     print(output.shape)
